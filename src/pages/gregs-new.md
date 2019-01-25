82263bd2c61f84008aefce9d3c8dd2e5d955cfb7
gnkatchmar <gunk55@msn.com>
2019-01-25 05:50:48 -0800

    part7 finished

---
+++ src/pages/gregs-new.md
@@ -0,0 +1,8 @@
+---
+title: "Greg and Pandas"
+date: "2019-01-25"
+---
+
+Greg added this new page.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0n3OepDn5GU" frameborder="0" allowfullscreen></iframe>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,10 @@
-// import React from "react"
-// import { Link } from "gatsby"
-// import Header from "../components/header"
-
-// export default () => (
-//   <div style={{ color: `purple` }}>
-//     <Link to="/contact/">Contact</Link> 
-//     <Header headerText="Hello Gatsby!" />
-//     <p>What a world.</p>
-//     <img src="https://source.unsplash.com/random/400x200" alt="" />
-//   </div>
-// )
-
-// import React from "react"
-
-// export default () => (
-//   <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-//     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-//     <p>
-//       What do I like to do? Lots of course but definitely enjoy building
-//       websites.
-//     </p>
-//   </div>
-// )
-
-// import React from "react"
-// import Layout from "../components/layout"
-
-// export default () => (
-//   <Layout> 
-//     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-//     <p>
-//       What do I like to do? Lots of course but definitely enjoy building
-//       websites.
-//     </p>
-//   </Layout>
-// )
-
-// import React from "react"
-// import Layout from "../components/layout"
-
-// export default () => (
-//   <Layout>
-//     <h1>Amazing Pandas Eating Things</h1>
-//     <div>
-//       <img
-//         src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-//         alt="Group of pandas eating bamboo"
-//       />
-//     </div>
-//   </Layout>
-// )
-
 import React from "react"
-import { graphql } from "gatsby"
 import { css } from "@emotion/core"
+import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <div>
@@ -73,21 +19,30 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
+                text-decoration: none;
+                color: inherit;
+              `
+}
             >
-              {node.frontmatter.title}{" "}
-              <span
+              <h3
                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link> 
           </div>
         ))}
       </div>
@@ -105,6 +60,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
